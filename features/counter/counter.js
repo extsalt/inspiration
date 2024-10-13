a8618297e9cd9d26eb95550cacbe3cef8a1f2f7e
Ganesh Gupta <ganeshk.work@gmail.com>
2024-10-13 14:11:43 +0530

    Add

--- features/counter/counter.js
+++ features/counter/counter.js
@@ -1,19 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { inspirationApi } from '@/services/inspiration/inspiration'
+import { createSlice } from '@reduxjs/toolkit'
 
 export const slice = createSlice({
-  name: "counter",
+  name: 'counter',
   initialState: {
     value: 0,
+    arr: [],
   },
   reducers: {
     increment: (state) => {
-      state.value += 1;
+      state.value += 1
     },
     decrement: (state) => {
-      state.value -= 1;
+      state.value -= 1
     },
   },
-});
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        inspirationApi.endpoints.getInspiration.matchFulfilled,
+        (state, action) => {
+          state.arr = [0, 0, 0]
+        }
+      )
+      .addMatcher(
+        inspirationApi.endpoints.getInspiration.matchPending,
+        (state, action) => {
+          state.arr = [-1, -1, -1]
+        }
+      )
+      .addMatcher(
+        inspirationApi.endpoints.getInspiration.matchRejected,
+        (state, action) => {
+          state.arr = [1, 1, 1]
+        }
+      )
+  },
+})
 
-export const { increment, decrement } = slice.actions;
-export default slice.reducer;
+export const { increment, decrement } = slice.actions
+export default slice.reducer
